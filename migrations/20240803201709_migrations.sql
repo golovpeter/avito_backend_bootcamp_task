a62d89a0868c7ac1a89867246d5e03bf990c618e
Golov Petr <petya-golov@mail.ru>
2024-08-10 02:41:54 +0300

    Addded trigger

--- migrations/20240803201709_migrations.sql
+++ migrations/20240803201709_migrations.sql
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS houses
 (
     id         BIGSERIAL PRIMARY KEY,
     address    VARCHAR(100) UNIQUE NOT NULL,
-    year       INT                NOT NULL,
-    developer  VARCHAR(20)        NOT NULL,
+    year       INT                 NOT NULL,
+    developer  VARCHAR(20)         NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
@@ -32,13 +32,39 @@ CREATE TABLE IF NOT EXISTS flats
 
     UNIQUE (number, house_id)
 );
+
+CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_flats_status ON flats (status);
+
+CREATE OR REPLACE FUNCTION update_houses_updated_at()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE houses
+    SET updated_at = NOW()
+    WHERE id = NEW.house_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trigger_update_houses_updated_at
+    AFTER INSERT OR UPDATE
+    ON flats
+    FOR EACH ROW
+EXECUTE FUNCTION update_houses_updated_at();
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX IF EXISTS idx_users_email;
+DROP INDEX IF EXISTS idx_flats_status;
+DROP TRIGGER IF EXISTS trigger_update_houses_updated_at ON flats;
+DROP TYPE IF EXISTS flat_status;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS flats;
 DROP TABLE IF EXISTS houses;
 DROP TYPE IF EXISTS user_role;
-DROP TYPE IF EXISTS flat_status;
 -- +goose StatementEnd
